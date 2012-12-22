7265dd835ac13a36b78763ebdbb4b2ce307dc95f
hpitit@gmail.com <hpitit@gmail.com@4e4afe1b-a2b6-eae1-34dc-357820034e6c>
2012-12-22 09:44:19 +0000

    + Up project desktop.

---
+++ Libgdx_GomBi_Desktop/src/com/vn/libgdx/mygame/gamelogic/MyTTPacker.java
@@ -0,0 +1,17 @@
+package com.vn.libgdx.mygame.gamelogic;
+/*Su dung:
+ *Chuot phai vao MyTTPacker
+ *Run as java application de pack hinh anh tu MyData sang MyDataPacker
+ * */
+
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
+
+public class MyTTPacker {
+	public static void main(String[] args) {
+		Settings tt = new Settings();
+		tt.maxWidth = 512;
+		tt.minWidth = 512;
+		TexturePacker2.process(tt, "MyData/", "MyDataPacker/", "FileMoTa.txt");
+	}
+}
